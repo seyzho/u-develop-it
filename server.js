628ea40f3cb15f9c85274b3400d57f6810306b6c
ShereKhan Adams <sherekhanadams@gmail.com>
2021-07-05 07:17:45 -0700

    sql queries

--- server.js
+++ server.js
@@ -21,6 +21,40 @@ const db = mysql.createConnection(
     console.log("connected to teh election database")
 );
 
+/*db.query(` SELECT * FROM candidates` , (err, rows) => {
+    console.log(rows);
+});
+*/
+
+/*GET a single candidate 
+db.query(`SELECT * FROM candidates WHERE id = 5`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+*/
+
+/* delete a candidate 
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+}); 
+*/
+
+/* create a candidate */
+const sql = `INSERT INTO  candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const  params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 //default response for any other requerst (not found)
 app.use((req,res) => {
