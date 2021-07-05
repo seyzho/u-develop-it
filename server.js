92db3e82add793af644a2b94dd1b62d081e8d571
ShereKhan Adams <sherekhanadams@gmail.com>
2021-07-05 06:15:45 -0700

    db connection functionality

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,20 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        //your mysql user
+        user: "root",
+        //your mysql pass
+        password: "Ultimate8!",
+        database: "election"
+    },
+    console.log("connected to teh election database")
+);
+
 
 //default response for any other requerst (not found)
 app.use((req,res) => {
