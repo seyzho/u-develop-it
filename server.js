898def980299ec75b8510b9e747e4afc2b917283
ShereKhan Adams <sherekhanadams@gmail.com>
2021-07-05 09:08:31 -0700

    put and stuff

--- server.js
+++ server.js
@@ -110,6 +110,88 @@ app.post("/api/candidate", ({ body }, res) => {
     });
 });
 
+//get parties
+app.get("/api/parties", (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
+
+//get parteis by id
+app.get("/api/party/:id", (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data:row
+        });
+    });
+});
+
+app.delete("/api/party/:id", (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            //checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: "party not found"
+            });
+        } else {
+            res.json({
+                message: "deleted",
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+//update a candidates party
+app.put("/api/candidate/:id", (req, res) => {
+    const errors = inputCheck(req.body, "party_id");
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `UPDATE candidates SET party_id = ?
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            //check if a record was fround
+        } else if (!result.affectedRows) {
+            res.json({
+                message: "candidate not found"
+            });
+        } else {
+            res.json({
+                message: "success",
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 //default response for any other requerst (not found)
 app.use((req,res) => {
     res.status(404).end();
